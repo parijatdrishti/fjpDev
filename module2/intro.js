bcb193cea20b634db1333a3a89f60e4afb0aa0cf
parijatdrishti <drishtiparijat04@gmail.com>
2022-03-05 22:11:20 +0530

    module2 started

---
+++ module2/intro.js
@@ -0,0 +1,42 @@
+// printing in js
+
+console.log("hello world!!!");
+
+// variables in js
+
+let num = 5; //Number
+console.log(num);
+
+let char = 'a'; //char
+console.log(char);
+
+let str = "I am string"; //string
+let string = 'I am also string'; //string
+
+console.log(str);
+console.log(string);
+
+let bool = 'true';
+console.log(bool);
+
+//loops
+for(let i = 1 ; i<=5 ; i++){
+    console.log(i);
+}
+
+let count = 10;
+while(count>0){
+    console.log(count);
+    count--;
+}
+
+// isprime
+let n = 17;
+let isprime = true;
+for(let i = 2 ; i*i <= n ; i++){
+    if(n%i == 0){
+        isprime = false;
+        break;
+    }
+}
+console.log(isprime);
