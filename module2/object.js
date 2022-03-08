3a67a7f0b0f22d9e12dceb2498c2915d23d016d4
parijatdrishti <drishtiparijat04@gmail.com>
2022-03-08 22:18:11 +0530

    object js

---
+++ module2/object.js
@@ -0,0 +1,26 @@
+// empty object
+let obj = {};
+console.log(obj);
+
+// initializing object
+let objName = {
+    Name : "xyv",
+    Age :34,
+    "phoneNo" : 49357390,
+    lastName : 'dkjie'
+};
+console.log(objName);
+
+let capAmerica = {
+    Name : "Steve",
+    Age : 9999,
+    Friends : ['Natasha','thor',"tony",'Bucky','Bruce'],
+    Address : {
+        City : "Queens",
+        State : 'Haryana'
+    },
+    sayHi : function(){
+        console.log("Cap America Says Hiii");
+    }
+}
+console.log(capAmerica);
